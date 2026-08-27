efa0d85488d531ecc1c9391151d4ad6caf7aec90
srtaintpullingover <deshunanderson727@gmail.com>
2026-08-26 23:57:46 -0500

    Update HistoryActivity.java

--- app/src/main/java/com/srt60/HistoryActivity.java
+++ app/src/main/java/com/srt60/HistoryActivity.java
@@ -1,8 +1,6 @@
 package com.srt60;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HistoryActivity extends AppCompatActivity {
@@ -11,23 +9,5 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-
-        View navHome = findViewById(R.id.histNavHome);
-        View navKeypad = findViewById(R.id.histNavKeypad);
-        View navHistory = findViewById(R.id.histNavHistory);
-
-        if (navHome != null) {
-            navHome.setOnClickListener(v -> {
-                startActivity(new Intent(HistoryActivity.this, HomeActivity.class));
-                finish();
-            });
-        }
-
-        if (navKeypad != null) {
-            navKeypad.setOnClickListener(v -> {
-                startActivity(new Intent(HistoryActivity.this, MainActivity.class));
-                finish();
-            });
-        }
     }
 }
