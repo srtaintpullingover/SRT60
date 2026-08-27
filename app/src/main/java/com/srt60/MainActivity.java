4a74fd86de24bfaeb292b96ec4dfd0c91a3baa05
srtaintpullingover <deshunanderson727@gmail.com>
2026-08-26 23:57:10 -0500

    Update MainActivity.java

--- app/src/main/java/com/srt60/MainActivity.java
+++ app/src/main/java/com/srt60/MainActivity.java
@@ -16,52 +16,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtAmount = (TextView) findViewById(R.id.txtAmount);
+        // Safe initialization with fallback if XML IDs differ
+        int resId =getResources().getIdentifier("txtAmount", "id", getPackageName());
+        if (resId != 0) {
+            txtAmount = (TextView) findViewById(resId);
+        }
 
-        int[] numButtonIds = {
-            R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
-            R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9, R.id.btnDot
-        };
-
+        // Setup generic click listener for keypad buttons safely by name
         View.OnClickListener numListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Button b = (Button) v;
-                String val = b.getText().toString();
+                if (v instanceof Button) {
+                    Button b = (Button) v;
+                    String val = b.getText().toString();
 
-                if (currentAmount.toString().equals("0") && !val.equals(".")) {
-                    currentAmount.setLength(0);
-                }
-                currentAmount.append(val);
-                if (txtAmount != null) {
-                    txtAmount.setText("$" + currentAmount.toString());
-                }
-            }
-        };
-
-        for (int i = 0; i < numButtonIds.length; i++) {
-            Button btn = (Button) findViewById(numButtonIds[i]);
-            if (btn != null) {
-                btn.setOnClickListener(numListener);
-            }
-        }
-
-        Button btnDel = (Button) findViewById(R.id.btnDel);
-        if (btnDel != null) {
-            btnDel.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (currentAmount.length() > 1) {
-                        currentAmount.deleteCharAt(currentAmount.length() - 1);
-                    } else {
+                    if (currentAmount.toString().equals("0") && !val.equals(".")) {
                         currentAmount.setLength(0);
-                        currentAmount.append("0");
                     }
+                    currentAmount.append(val);
                     if (txtAmount != null) {
                         txtAmount.setText("$" + currentAmount.toString());
                     }
                 }
-            });
+            }
+        };
+
+        // Bind numbers 0-9 and dot dynamically without hardcoding missing R.id references
+        String[] btnNames = {"btn0", "btn1", "btn2", "btn3", "btn4", "btn5", "btn6", "btn7", "btn8", "btn9", "btnDot"};
+        for (int i = 0; i < btnNames.length; i++) {
+            int id = getResources().getIdentifier(btnNames[i], "id", getPackageName());
+            if (id != 0) {
+                View btn = findViewById(id);
+                if (btn != null) {
+                    btn.setOnClickListener(numListener);
+                }
+            }
+        }
+
+        // Safe delete button bind
+        int delId = getResources().getIdentifier("btnDel", "id", getPackageName());
+        if (delId != 0) {
+            View btnDel = findViewById(delId);
+            if (btnDel != null) {
+                btnDel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (currentAmount.length() > 1) {
+                            currentAmount.deleteCharAt(currentAmount.length() - 1);
+                        } else {
+                            currentAmount.setLength(0);
+                            currentAmount.append("0");
+                        }
+                        if (txtAmount != null) {
+                            txtAmount.setText("$" + currentAmount.toString());
+                        }
+                    }
+                });
+            }
         }
     }
 }
