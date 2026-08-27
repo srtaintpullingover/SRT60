18e0a55db32dd4c0878ddc5f54305824cbf726a8
srtaintpullingover <deshunanderson727@gmail.com>
2026-08-26 22:28:14 -0500

    Update MainActivity.java

--- app/src/main/java/com/srt60/MainActivity.java
+++ app/src/main/java/com/srt60/MainActivity.java
@@ -1,12 +1,24 @@
 package com.srt60;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHistory = findViewById(R.id.btnHistory);
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
